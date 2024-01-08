92477c140f7334920517fb341bef8c170888b53c
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-08 06:50:15 +0530

    Exit Status Check

--- jenkins-ip-update/main.tf
+++ jenkins-ip-update/main.tf
@@ -31,6 +31,18 @@ resource "aws_route53_record" "artifactory" {
   records = [data.aws_instance.artifactory.public_ip]
 }
 
+data "aws_instance" "elk" {
+  instance_id = "i-0c5e20f1c334b6458"
+}
+
+resource "aws_route53_record" "elk" {
+  name    = "elasticsearch"
+  type    = "A"
+  zone_id = "Z0021413JFIQEJP9ZO9Z"
+  ttl     = 10
+  records = [data.aws_instance.elk.public_ip]
+}
+
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
